c0e87d520549903352c04626a104edb031305325
Desicated375 <144464721+Desicated375@users.noreply.github.com>
2024-07-03 10:21:42 +0300

    Pract4
    proga

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    int numb = 0;
+    printf("Enter a natural number n: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+        {
+        if (n % i == n / i)
+            {
+            numb++;
+        }
+    }
+
+    printf("The number of equal divisors of a number %d: %d\n", n, numb);
+
+    return 0;
+}
